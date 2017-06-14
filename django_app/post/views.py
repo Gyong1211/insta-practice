be9b9310dcde076853fbf761896d2ba537d19884
Gihong Kim <gyong1211@gmail.com>
2017-06-14 12:25:34 +0900

    add post urls and views

--- django_app/post/views.py
+++ django_app/post/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def post_list(request):
+    return HttpResponse('Post List')
+
+
+def post_detail(request, post_pk):
+    return HttpResponse('Post Detail')

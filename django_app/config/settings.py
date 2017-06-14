24a7b37f80818277e5dd772e075777c7af96abf3
Gihong Kim <gyong1211@gmail.com>
2017-06-14 11:59:36 +0900

    add member & post app

--- django_app/config/settings.py
+++ django_app/config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'member',
+    'post',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [

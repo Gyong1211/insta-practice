cd79ca9874027ab32cdf0b49a52c8e520d16355c
Gihong Kim <gyong1211@gmail.com>
2017-06-14 20:57:14 +0900

    add post_create(views, urls, template)

--- django_app/post/forms.py
+++ django_app/post/forms.py
@@ -2,9 +2,9 @@ from django import forms
 
 
 class CreatePost(forms.Form):
-    image = forms.ImageField()
+    photo = forms.ImageField()
     comment = forms.CharField(max_length=100, required=False)
 
 
 class ModifyPost(forms.Form):
-    image = forms.ImageField()
+    photo = forms.ImageField()
--- django_app/post/views.py
+++ django_app/post/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from .models import Post
+from member.models import User
+from .forms import CreatePost
+from .models import Post, Comment
 
 
 def post_list(request):
@@ -18,3 +20,27 @@ def post_detail(request, post_pk):
         'post': post,
     }
     return render(request, 'post/post_detail.html', context=context)
+
+
+def post_create(request):
+    if request.method == 'POST':
+        forms = CreatePost(request.POST, request.FILES)
+        if forms.is_valid():
+            user = User.objects.get(id=2)
+            post = Post.objects.create(author=user, photo=request.FILES['photo'])
+            comment_string = forms.cleaned_data['comment']
+            if comment_string:
+                post.comment_set.create(author=user, content=comment_string)
+            return redirect('post:post_detail', post_pk=post.id)
+        else:
+            forms = CreatePost()
+            context = {
+                'forms': forms,
+            }
+            return render(request, 'post/post_create.html', context=context)
+    else:
+        forms = CreatePost()
+        context = {
+            'forms': forms,
+        }
+        return render(request, 'post/post_create.html', context=context)
